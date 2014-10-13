b5c56f69a927daca0ab65a60e444b53b0bdde9b8
Roger Lee <rcmlee99@gmail.com>
2014-10-13 14:31:42 +1100

    Eugene changes

--- android/src/main/java/com/google/samples/apps/iosched/Config.java
+++ android/src/main/java/com/google/samples/apps/iosched/Config.java
@@ -35,7 +35,7 @@ public class Config {
     public static final String DOGFOOD_BUILD_WARNING_TEXT = "This is a test build.";
 
     // Public data manifest URL
-    public static final String PROD_CONFERENCE_DATA_MANIFEST_URL = "";
+    public static final String PROD_CONFERENCE_DATA_MANIFEST_URL = "http://my-mobile-demos.s3.amazonaws.com/manifest.json";
 
     // Manifest URL override for Debug (staging) builds:
     public static final String MANIFEST_URL = PROD_CONFERENCE_DATA_MANIFEST_URL;
@@ -82,7 +82,7 @@ public class Config {
     public static final String ANNOUNCEMENTS_PLUS_ID = "";
 
     // YouTube API config
-    public static final String YOUTUBE_API_KEY = "";
+    public static final String YOUTUBE_API_KEY = "AIzaSyCNv4Z2aA_HQHA5UEg69kBRWPLAChHfaSs";
 
     // YouTube share URL
     public static final String YOUTUBE_SHARE_URL_PREFIX = "http://youtu.be/";
